d4214d9bb7b12830ef6a25680a507086d2bde730
chaninkou <michaelkoulove@yahoo.com>
2019-12-21 23:29:56 -0800

    Great review on rotating an array.

--- LeetCode-Rotate-Array.git/src/rotate/RotateArrayFunction.java
+++ LeetCode-Rotate-Array.git/src/rotate/RotateArrayFunction.java
@@ -4,25 +4,30 @@ import java.util.Arrays;
 
 public class RotateArrayFunction {
     public static void rotate(int[] nums, int k) {
+    	// The reason why we should mod because each element 
         k = k % nums.length;
         
         // reverse "----->-->" will be "<--<-----"
+        // First reverse the array
         reverse(nums, 0, nums.length - 1);
         
         System.out.println("first reverse: " + Arrays.toString(nums));
         
         // reverse "<--" will be "--><-----"
+        // Then swap left and right within the k limit
         reverse(nums, 0, k - 1);
         
         System.out.println("second reverse: " + Arrays.toString(nums));
         
         // reverse "<-----" we can get "-->----->"
+        // Then reverse back the element after k
         reverse(nums, k, nums.length - 1);
         
         System.out.println("Solution: " + Arrays.toString(nums));
     }
     
     public static void reverse(int[] nums, int start, int end){
+    	// Swap beginning and end element
         while(start < end){
             int temp = nums[start];
             
